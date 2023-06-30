3f55fdd222ddd036e5127b1032d1299cb5231410
phil <philfanzhou@hotmail.com>
2023-06-30 15:11:18 +0800

    clean code.

--- Qz.Infra.Database/Condition/WhereConditionsItem.cs
+++ Qz.Infra.Database/Condition/WhereConditionsItem.cs
@@ -1,22 +1,21 @@
 ﻿using Qz.Infra.Database.Params;
 
-namespace Qz.Infra.Database.Condition
+namespace Qz.Infra.Database.Condition;
+
+public class WhereConditionsItem : WhereConditionsItemBase
 {
-    public class WhereConditionsItem : WhereConditionsItemBase
+    public WhereConditionsItem(WhereParamsItem paramsItem,
+        WhereOperator whereOperator = WhereOperator.Equal,
+        WhereConnecter connecter = WhereConnecter.And)
     {
-        public WhereConditionsItem(WhereParamsItem paramsItem,
-            WhereOperator whereOperator = WhereOperator.Equal,
-            WhereConnecter connecter = WhereConnecter.And)
-        {
-            Param = paramsItem;
-            WhereOperator = whereOperator;
-            WhereConnecter = connecter;
-        }
-
-        public WhereParamsItem Param { get; }
-
-        public WhereOperator WhereOperator { get; protected set; }
-
-        public override string Column => Param?.Column;
+        Param = paramsItem;
+        WhereOperator = whereOperator;
+        WhereConnecter = connecter;
     }
+
+    public WhereParamsItem Param { get; }
+
+    public WhereOperator WhereOperator { get; protected set; }
+
+    public override string Column => Param?.Column;
 }
