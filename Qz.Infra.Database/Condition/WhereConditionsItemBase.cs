9135d1f941e865dbd1dd1ea748480247556f4b64
phil <philfanzhou@hotmail.com>
2023-06-30 15:03:06 +0800

    fix sql param name same issue.

---
+++ Qz.Infra.Database/Condition/WhereConditionsItemBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Qz.Infra.Database.Condition;
+
+public abstract class WhereConditionsItemBase
+{
+    public abstract string Column { get; }
+
+    public WhereConnecter WhereConnecter { get; protected set; }
+}
---
+++ Qz.Infra.Database/Condition/WhereConditionsNullItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Qz.Infra.Database.Condition;
+
+public class WhereConditionsNullItem : WhereConditionsItemBase
+{
+    public WhereConditionsNullItem(string column, bool isNull,
+        WhereConnecter whereConnecter = WhereConnecter.And)
+    {
+        Column = column;
+        IsNull = isNull;
+        WhereConnecter = whereConnecter;
+    }
+
+    public bool IsNull { get; }
+
+    public override string Column { get; }
+}
