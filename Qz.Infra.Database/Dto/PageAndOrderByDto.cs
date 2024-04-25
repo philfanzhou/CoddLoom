e6f1966b0e6bf85c29375aa49eb9934ace613a53
phil <philfanzhou@hotmail.com>
2024-04-25 22:43:57 +0800

    update query dto.

--- Qz.Infra.Database/Dto/PageAndOrderByDto.cs
+++ Qz.Infra.Database/Dto/PageAndOrderByDto.cs
@@ -1,11 +1,7 @@
 ﻿namespace Qz.Infra.Database.Dto;
 
-public class PageAndOrderByDto : IPageQuery, IOrderByQuery
+public class PageAndOrderByDto : PageQueryDto, IOrderByQuery
 {
-    public int PageNumber { get; set; } = 1;
-
-    public int PageSize { get; set; } = 20;
-
     public string OrderBy { get; set; }
 
     public bool IsDesc { get; set; }
