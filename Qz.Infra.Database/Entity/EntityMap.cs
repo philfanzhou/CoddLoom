9e03dc6b7335b5d4c9b8292b34e487858247ad8d
phil <philfanzhou@hotmail.com>
2024-07-15 15:33:45 +0800

    fix issue.

--- Qz.Infra.Database/Entity/EntityMap.cs
+++ Qz.Infra.Database/Entity/EntityMap.cs
@@ -16,13 +16,13 @@ internal class EntityMap
         {
             throw new ArgumentOutOfRangeException(nameof(type));
         }
-
+        var names = new HashSet<string>();
         var memberList = new List<Tuple<MemberInfo, MapColumnAttribute>>();
         var members = type.GetAllMembers();
         foreach (var member in members)
         {
             var attribute = member.GetCustomAttribute<MapColumnAttribute>(true);
-            if (attribute != null)
+            if (attribute != null && names.Add(member.Name))
             {
                 memberList.Add(new Tuple<MemberInfo, MapColumnAttribute>(member, attribute));
                 if (attribute.PrimaryKey)
