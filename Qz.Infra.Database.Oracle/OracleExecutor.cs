27c25713a8f3c126aeb01b52b1fc4a1becbef520
phil <philfanzhou@hotmail.com>
2024-11-29 09:04:18 +0800

    add oracle.

---
+++ Qz.Infra.Database.Oracle/OracleExecutor.cs
@@ -0,0 +1,35 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using Qz.Infra.Database.Condition;
+using Qz.Infra.Database.Table;
+using System;
+using System.Data;
+
+namespace Qz.Infra.Database.Oracle;
+
+public class OracleExecutor : DbExecutor
+{
+    public OracleExecutor(string connectionString)
+        : base(connectionString, new OracleConnection(connectionString))
+    {
+    }
+
+    public override IDbConnection GetConnection()
+    {
+        return new OracleConnection(ConnectionString);
+    }
+
+    protected override Func<string, object, IDbDataParameter> GetAddParameterFunc(IDbCommand command)
+    {
+        if(command is not OracleCommand cmd)
+        {
+            throw new ArgumentOutOfRangeException(nameof(command));
+        }
+
+        return cmd.Parameters.Add;
+    }
+
+    protected override void GetExistTableParam(TableDefine table, out string checkTable, out WhereConditions where)
+    {
+        throw new NotImplementedException();
+    }
+}
