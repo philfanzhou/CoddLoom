b62240ad18026f2212e7e1cec87c3251f9735dee
phil <philfanzhou@hotmail.com>
2025-04-06 14:40:46 +0800

    add pageresult.

---
+++ Qz.Infra.Database/Model/PageResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Qz.Infra.Database.Model;
+
+public class PageResult<T>
+{
+    public List<T> Items { get; set; }
+
+    public int PageNumber { get; set; }
+
+    public int TotalPage { get; set; }
+
+    public int TotalCount { get; set; }
+}
